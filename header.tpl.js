13a4faeb0162a114214c210f0dad8a3146549be5
MarieLeFloch <marie-le.floch@hotmail.fr>
2022-05-09 17:44:31 +0200

    ajout page about

---
+++ header.tpl.js
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html lang="fr"></html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MarieIsTravelling</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/fonts.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <script src="https://kit.fontawesome.com/bc0b65ddeb.js" crossorigin="anonymous"></script>
+</head>
